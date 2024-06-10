1a261e1b3e850cdd227763c9c2506a232ea50a1c
Markuss <markuss.jansons@sk.lvt.lv>
2024-06-10 12:30:33 +0300

    Pievienoti jautājumi ar atbildēm

---
+++ Jautajumi.py
@@ -0,0 +1,101 @@
+def jautajumi():
+    jautajumi = [
+        {
+            "jautajums": "Kapēc sazarojuma konstrukcija eksistē? ", 
+            "opcijas": 
+                ["A. Sazarojuma konstrukcija eksistē, lai pārbaudītu noteiktus nosacījumus un, "
+                    "izpildītu kodu atkarībā no tā, vai šie nosacījumi ir patiesi vai nepatiesi.",
+                 "B. ",
+                 "C. ",
+                 "D. "   ],
+                 "atbilde": "A"
+        },
+        {
+            "jautajums": "Sazarojuma konstrukcijas operātori ir ", 
+            "opcijas": 
+                ["A. isnt, is, was ",
+                 "B. and, or, not ",
+                 "C. maybe, sometimes, dont",
+                 "D. Loģisko operātoru nepastāv sazarojuma konstrukcijās."   ],
+                 "atbilde": "B"
+        },
+        {
+            "jautajums": "Sazarojumus pitonā pieraksta ar ", 
+            "opcijas": 
+                ["A. if - else if - else " ,
+                 "B. if... elsewhere - if... it isnt - if... was",
+                 "C. if - if... else - elif",
+                 "D. ja - ja... savādāk - savādāk ja"   ],
+                 "atbilde": "C"
+        },
+        {
+            "jautajums": "a = 1\nb = 3\nif b > a:\n    print(str(b) + \" ir lielāks nekā \" + str(a))", 
+            "opcijas \n Kas izvadas konsolē? ": 
+                ["A. 1 ir lielāks nekā 1",
+                 "B. 3 ir lielāks nekā 3",
+                 "C. 1 ir lielāks nekā 3",
+                 "D. 3 ir lielāks nekā 1"   ],
+                 "atbilde": "D"
+        },
+        {
+            "jautajums": "Kurš salīdzināšanas operātors nepastāv? ", 
+            "opcijas": 
+                ["A. a !=! b ",
+                 "B. a == b",
+                 "C. a < b",
+                 "D. a <= b"   ],
+                 "atbilde": "A"
+        },
+        {
+            "jautajums": "vārds = 'Markuss'\nif vārds == 'Markuss':\n    print(\"Čau Markuss!\")\nelse:\n "
+                  "print(\"Atvaino ... es tevi nepazīstu\") \n Kas tiks izdrukāts konsolē?",
+            "opcijas": 
+               ["A. Čau!", 
+                "B. Čau Markuss!", 
+                "C. Atvaino... es tevi nepazīstu", 
+                "D. Nekas, programma nestrādā. "],
+            "atbilde": "B"
+}
+
+        {
+            "jautajums": "vecums = 18 \n if vecums >= 18: "
+            "\n \bprint(\"Jūs esat balsstiesīgs\") \n else: \n \bprint(\"Jums nav balsstiesības\") \n Kas tiks izdrukāts ekrānā? ) ", 
+            "opcijas": 
+                ["A. 18",
+                 "B. Jums nav balsstiesības",
+                 "C. Jūs esat balsstiesīgs",
+                 "D. Nekas netiks izdrukāts."   ],
+                 "atbilde": "C"
+        },
+        {
+            "jautajums": "Kāda ir if vispārīga pierakstu forma? ", 
+            "opcijas": 
+                ["A. if <izpildāmās komandas>: \n\b<nosacījums>",
+                 "B. elif <nosacījums>: \n\b<izpildāmās komandas>",
+                 "C. if <nosacījums>: \n\b<nosacījums2>",
+                 "D. if <nosacījums>: \n\b<izpildāmās komandas>"   ],
+                 "atbilde": "D"
+        },
+        {
+            "jautajums": "Kāda ir if else vispārīga pierakstu forma? ", 
+            "opcijas": 
+                ["A. if <nosacījums>: \n\b<izpildāmās komandas, ja nosacījums patiess>"
+                     "\nelse: \n\b<izpildāmās komandas, ja nosacījums ir aplams>",
+                 "B. if <nosacījums>: \n\b<izpildāmās komandas, ja nosacījums patiess>"
+                     "\nelse: \n\b<izpildāmās komandas, ja nosacījums ir patiess>",
+                 "C. if <nosacījums>: \n\b<izpildāmās komandas, ja nosacījums aplams>"
+                     "\nelse: \n\b<izpildāmās komandas, ja nosacījums ir patiess>",
+                 "D. if <izpildāmās komandas>: \n\b<nosacījums, ja nosacījums patiess>"
+                     "\nelse: \n\b<izpildāmās komandas, ja nosacījums ir aplams>"   ],
+                 "atbilde": "A"
+        },
+        {
+            "jautajums": "Vai var izmantot tikai if konstrukciju bez else un elif? ", 
+            "opcijas": 
+                ["A. Nē",
+                 "B. Jā",
+                 "C. Pitonā nevar, java var. ",
+                 "D. Citreiz programma ļauj, citreiz nē."   ],
+                 "atbilde": "B"
+        },
+    ]
